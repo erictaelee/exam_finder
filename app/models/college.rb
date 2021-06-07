1dca1423e5f42d3594afb1996da48326ee64acb4
Eric <lee.erictae@gmail.com>
2021-06-07 00:21:35 -0500

    set associations

--- app/models/college.rb
+++ app/models/college.rb
@@ -1,2 +1,4 @@
 class College < ApplicationRecord
+  has_many :users, through: :exams
+  has_many :exams
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  belongs_to :college
+  belongs_to :exam
 end
