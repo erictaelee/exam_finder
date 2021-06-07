2caa25a7ce6ac50dde664b46865ecc653b18f252
Eric <lee.erictae@gmail.com>
2021-06-07 00:37:12 -0500

    add create action

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,4 +8,16 @@ class Api::UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     render "show.json.jb"
   end
+
+  def create
+    @user = User.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      college_id: @user.college_id,
+      exam_id: @user.exam_id
+    )
+    @user.save
+    render 'show.jason.jb'
+  end
 end
