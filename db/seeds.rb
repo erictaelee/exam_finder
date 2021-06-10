1ab74a426ec193c4d95411c24d8155485e7182f0
Eric <lee.erictae@gmail.com>
2021-06-09 20:29:34 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 
-user = User.create(first_name: "John", last_name: "Doe", phone_number: "199-999-9999", college_id: 5, exam_id: 11)
+user = User.create(first_name: "Derrick", last_name: "Rose", phone_number: "199-999-9999", college_id: 4)
 
-college = College.create(name: "NIU")
+# college = College.create(name: "NIU")
+# college = College.create(name: "UIC")
+# college = College.create(name: "UIUC")
 
-exam_window = ExamWindow.create(start_time: "2021/7/14 11:00:00")
+# exam_window = ExamWindow.create(start_time: "2021/7/14 11:00:00")
